20fe6ddea01d0624e0f9f6ce0a4239188922cd66
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-18 09:33:39 +0100

    refactoring

--- src/main/java/io/electrosalaf/bankly/web/TransactionServlet.java
+++ src/main/java/io/electrosalaf/bankly/web/TransactionServlet.java
@@ -1,17 +1,22 @@
 package io.electrosalaf.bankly.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.electrosalaf.bankly.context.Application;
 import io.electrosalaf.bankly.model.Transaction;
+import io.electrosalaf.bankly.service.TransactionService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
 public class TransactionServlet extends HttpServlet {
+
+    private TransactionService transactionService = Application.transactionService;
+    private ObjectMapper objectMapper = Application.objectMapper;
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -24,8 +29,7 @@ public class TransactionServlet extends HttpServlet {
             Transaction transaction = Application.transactionService.create(amount, reference);
 
             response.setContentType("application/json; charset=UTF-8");
-            String json = Application.objectMapper.writeValueAsString(transaction);
-            response.getWriter().print(json);
+            response.getWriter().print(objectMapper.writeValueAsString(transaction));
         }
         else {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -35,20 +39,12 @@ public class TransactionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if (request.getRequestURI().equalsIgnoreCase("/")) {
-            response.setContentType("text/html; charset=UTF-8");
-            response.getWriter().print(
-                    "<html>\n" +
-                            "<body>\n" +
-                            "<h1>Bankly App</h1>\n" +
-                            "<p>Welcome to Banking App, You'll get all your transactions shortly</p>\n" +
-                            "</body>\n" +
-                            "</html>"
-            );
-        } else if (request.getRequestURI().equalsIgnoreCase("/transactions")) {
+        if (request.getRequestURI().equalsIgnoreCase("/transactions")) {
+            List<Transaction> transactions = transactionService.findAll();
             response.setContentType("application/json; charset=UTF-8");
-            List<Transaction> transactions = Application.transactionService.findAll();
-            response.getWriter().print(Application.objectMapper.writeValueAsString(transactions));
+            response.getWriter().print(objectMapper.writeValueAsString(transactions));
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
     }
 }
