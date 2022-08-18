572ec5a4ad1e8db04533de0ce5c4dd49d5c3cf8c
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-18 09:32:46 +0100

    refactoring

--- src/main/java/io/electrosalaf/bankly/ApplicationLauncher.java
+++ src/main/java/io/electrosalaf/bankly/ApplicationLauncher.java
@@ -9,8 +9,15 @@ import org.apache.catalina.startup.Tomcat;
 public class ApplicationLauncher {
 
     public static void main(String[] args) throws LifecycleException {
+
+        int serverPort = 8080;
+        String portProperty = System.getProperty("server.port");
+        if (portProperty != null) {
+            serverPort = Integer.parseInt(portProperty);
+        }
+
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+        tomcat.setPort(serverPort);
         tomcat.getConnector();
 
         Context appContext = tomcat.addContext("", null);
@@ -18,7 +25,7 @@ public class ApplicationLauncher {
         // Add Servlet to Tomcat
         Wrapper servlet = tomcat.addServlet(appContext, "transactionServlet", new TransactionServlet());
         servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
+        servlet.addMapping("/*");
 
         tomcat.start();
     }
